90d9e51783b5e597bf14a45af6429df8dcaa9108
KamilAdd-Byte <kamillodzinski09@gmail.com>
2021-11-01 20:10:56 +0100

    0.2 Generete component navbar, footer and container. Import mdb-bootstrap and mat-card to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatChipsModule} from '@angular/material/chips';
+import {MDBBootstrapModule} from 'angular-bootstrap-md';
+import { FooterComponent } from './footer/footer.component';
+import { ContainerComponent } from './container/container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    ContainerComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MDBBootstrapModule.forRoot(),
+    MatButtonModule,
+    MatFormFieldModule,
+    MatChipsModule,
+    MatGridListModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
